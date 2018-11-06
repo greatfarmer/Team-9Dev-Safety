9391ab5396e2908a4102fe27586675f69066ef6c
companyBean <sbean0215@smartlo.net>
2018-11-06 19:03:01 +0900

    임시커밋

---
+++ app/src/main/java/alpha/a9dev/team/safetyroadhelper/GlobalApplication.java
@@ -0,0 +1,10 @@
+package alpha.a9dev.team.safetyroadhelper;
+
+import android.support.multidex.MultiDexApplication;
+
+public class GlobalApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
